8abf35f5f72f46a0dc8a9bf5b7935e46fbd4aaa1
Edward <edvards.zelis@blockvis.com>
2021-01-12 17:12:30 +0200

    added new functions

--- solana_rpc_test.go
+++ solana_rpc_test.go
@@ -2,7 +2,10 @@ package parsiq_solana_hclient
 
 import (
 	"fmt"
+	"github.com/stretchr/testify/assert"
+	"net/http"
 	"testing"
+	"time"
 )
 
 const (
@@ -10,17 +13,23 @@ const (
 )
 
 func TestGetConfirmedBlock(t *testing.T) {
+
 	client := NewSolanaRpcClient(testApiRpcAddr)
-	resp, err := client.GetConfirmedBlock(59_211_174)
+	epoch, _ := client.GetEpochInfo()
+	resp, err := client.GetConfirmedBlock(epoch.Result.AbsoluteSlot)
 	if err != nil {
 		panic(err)
 	}
+	assert.NotEqual(t, resp.Result.BlockTime, nil, "block time is nil")
+	assert.NotEqual(t, resp.Result.Blockhash, nil, "blockhash is nil")
+	assert.NotEqual(t, resp.Result.Rewards, nil, "block time is nil")
 	fmt.Printf("%+v\n", resp.Result)
 }
 
 func TestGetConfirmedBlockWithLimit(t *testing.T) {
 	client := NewSolanaRpcClient(testApiRpcAddr)
-	resp, err := client.GetConfirmedBlocksWithLimit(59_212_174, 100)
+	epoch, _ := client.GetEpochInfo()
+	resp, err := client.GetConfirmedBlocksWithLimit(epoch.Result.AbsoluteSlot, 100)
 	if err != nil {
 		panic(err)
 	}
@@ -34,6 +43,9 @@ func TestGetEpochInfo(t *testing.T) {
 		panic(err)
 	}
 	fmt.Printf("%+v\n", resp.Result)
+	assert.NotEqual(t, resp.Result.Epoch, nil, "epoch is nil")
+	assert.NotEqual(t, resp.Result.AbsoluteSlot, nil, "absolute slot is nil")
+	assert.NotEqual(t, resp.Result.SlotsInEpoch, nil, "slots in epoch is nil")
 }
 
 func TestGetAccountInfo(t *testing.T) {
@@ -41,7 +53,7 @@ func TestGetAccountInfo(t *testing.T) {
 		Encoding: "base64",
 	}
 	client := NewSolanaRpcClient(testApiRpcAddr)
-	resp, err := client.GetAccountInfo("6h9jyRgfpmgXNyaWpbDpbxbCoF56WEbzsruhMwDn2om4", params)
+	resp, err := client.GetAccountInfo("4fYNw3dojWmQ4dXtSGE9epjRGy9pFSx62YypT7avPYvA", params)
 	if err != nil {
 		panic(err)
 	}
@@ -60,6 +72,74 @@ func TestGetFirstAvailableBlock(t *testing.T) {
 func TestGetGenesisHash(t *testing.T) {
 	client := NewSolanaRpcClient(testApiRpcAddr)
 	resp, err := client.GetGenesisHash()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%+v\n", resp.Result)
+}
+
+func TestGetBalance(t *testing.T) {
+	client := NewSolanaRpcClient(testApiRpcAddr)
+	commitment := &Commitment{}
+	commitment.Commitment = "max"
+	resp, err := client.GetBalance("GK2zqSsXLA2rwVZk347RYhh6jJpRsCA69FjLW93ZGi3B", commitment)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%+v\n", resp.Result)
+}
+
+func TestGetClusterNodes(t *testing.T) {
+	client := NewSolanaRpcClient(testApiRpcAddr)
+	resp, err := client.GetClusterNodes()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%+v\n", resp.Result[0])
+}
+
+func TestGetLargestAccounts(t *testing.T) {
+	params := &LargestAccountsParams{
+		Commitment: "max",
+		Filter:     "nonCirculating",
+	}
+	client := &http.Client{
+		Transport: &http.Transport{
+			IdleConnTimeout:       5 * time.Minute,
+			MaxIdleConnsPerHost:   100,
+			ResponseHeaderTimeout: time.Second * 30,
+		},
+	}
+	custom := NewCustomSolanaRpcClient(testApiRpcAddr, client)
+	resp, err := custom.GetLargestAccounts(params)
+	if err != nil {
+		panic(err)
+	}
+	//fmt.Printf("%+v\n", resp.Result)
+	params = &LargestAccountsParams{
+		Commitment: "max",
+		Filter:     "circulating",
+	}
+
+	resp2, err := custom.GetLargestAccounts(params)
+	if err != nil {
+		panic(err)
+	}
+	assert.NotEqual(t, resp, resp2, "objects are identical")
+}
+
+func TestGetFees(t *testing.T) {
+	client := NewSolanaRpcClient(testApiRpcAddr)
+	resp, err := client.GetFees(&Commitment{Commitment: "max"})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%+v\n", resp.Result)
+}
+
+func TestSimulateTransaction(t *testing.T) {
+	client := NewSolanaRpcClient(testApiRpcAddr)
+	resp, err := client.SimulateTransaction("4hXTCkRzt9WyecNzV1XPgCDfGAZzQKNxLXgynz5QDuWWPSAZBZSHptvWRL3BjCvzUXRdKvHL2b7yGrRQcWyaqsaBCncVG7BFggS8w9snUts67BSh3EqKpXLUm5UMHfD7ZBe9GhARjbNQMLJ1QD3Spr6oMTBU6EhdB4RD8CP2xUxr2u3d6fos36PD98XS6oX8TQjLpsMwncs5DAMiD4nNnR8NBfyghGCWvCVifVwvA8B8TJxE1aiyiv2L429BCWfyzAme5sZW8rDb14NeCQHhZbtNqfXhcp2tAnaAT")
 	if err != nil {
 		panic(err)
 	}
